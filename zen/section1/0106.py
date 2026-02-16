79a175635796266d0f3719272713f808b9011a0f
Yuka Hashio <your.email@example.com>
2026-02-16 10:12:27 +0900

    add section1

---
+++ zen/section1/0106.py
@@ -0,0 +1,28 @@
+'''
+zen.section1.0106 の Docstring
+演習問題
+'''
+
+import math
+
+# pi
+'''
+直径 30 cm のピザの面積を求めてください。
+'''
+pizza_radius = 30 // 2
+pizza_area = math.pi * pizza_radius ** 2
+print(pizza_area)
+
+
+# sqrt
+'''
+大型のウサギを飼うには、床面積が 5400 cm2 のケージが必要です。
+床が正方形だとすると、一辺の長さは何 cm になるでしょう？
+'''
+print(math.sqrt(5400))
+
+
+### check math module ###
+# python3
+# help(math)
+
