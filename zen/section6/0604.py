447eee5dea4c7beabfe8336db5502ac3779a7f35
Yuka Hashio <251zs03753@student.zen.ac.jp>
2026-02-18 01:50:57 +0900

    add code

---
+++ zen/section6/0604.py
@@ -0,0 +1,21 @@
+'''
+zen.section6.0604 の Docstring
+PP_06_04_リストのメソッド.ipynb
+'''
+
+
+
+# reverse
+'''
+reverse メソッドを使って、 iroha リストの要素を逆順に並べ替えてください。
+リストを表示して、中身を確かめてください。
+'''
+
+
+
+# index
+
+
+
+
+# copy
