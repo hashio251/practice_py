bf696959c350f46e6872853d7dda5c688d1caa65
Yuka Hashio <251zs03753@student.zen.ac.jp>
2026-02-18 01:58:10 +0900

    add code

---
+++ zen/section6/0605.py
@@ -0,0 +1,40 @@
+'''
+zen.section6.0605 の Docstring
+PP_06_05_リストの編集.ipynb
+'''
+
+
+# 要素の変更
+
+
+
+
+# append
+
+
+
+
+# extend
+
+
+
+
+# insert
+
+
+
+# remove
+
+
+
+
+# pop
+
+
+
+
+# stacck
+
+
+
+# del
