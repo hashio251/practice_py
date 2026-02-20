a68659a21b46aece88b27b3ddc52d20fc48ba242
Yuka Hashio <251zs03753@student.zen.ac.jp>
2026-02-21 01:53:50 +0900

    add code

--- zen/section5/0502.py
+++ zen/section5/0502.py
@@ -12,7 +12,13 @@ PP_05_02_引数を受け取る関数.ipynb
 仮引数の名前は、 age などを使うとよいでしょう。
 age (エイジ) は、年齢という意味です。
 '''
+def send_birthday_message(age):
+  return f"{age}歳のお誕生日おめでとう！素敵な一年になりますように！"
 
+age = int(input("何歳になりましたか？"))
+birthday_message = send_birthday_message(age)
+
+print(birthday_message)
 
 
 
@@ -20,7 +26,16 @@ age (エイジ) は、年齢という意味です。
 '''
 ドックストリングを使って、誕生日のメッセージを表示する関数に説明を加えてください。
 '''
+def send_birthday_message(age):
+  return f"{age}歳のお誕生日おめでとう！素敵な一年になりますように！"
 
+age = int(input("何歳になりましたか？"))
+'''
+ここで誕生日のメッセージを表示させる関数を呼び出し変数に代入
+'''
+birthday_message = send_birthday_message(age)
+
+print(birthday_message)
 
 
 
@@ -28,3 +43,16 @@ age (エイジ) は、年齢という意味です。
 '''
 誕生日のメッセージを表示する関数のドックストリングに、引数の説明を加えてください。
 '''
+def send_birthday_message(age):
+  return f"{age}歳のお誕生日おめでとう！素敵な一年になりますように！"
+
+'''
+intの型を指定してユーザーからの入力を受け取ってageという変数に代入
+'''
+age = int(input("何歳になりましたか？"))
+'''
+ここで誕生日のメッセージを表示させる関数を呼び出し変数に代入
+'''
+birthday_message = send_birthday_message(age)
+
+print(birthday_message)
