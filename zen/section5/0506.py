fe2c4b08b08484034f6e38932e779bf91e46d4d4
Yuka Hashio <251zs03753@student.zen.ac.jp>
2026-02-22 21:09:17 +0900

    add code

--- zen/section5/0506.py
+++ zen/section5/0506.py
@@ -2,3 +2,18 @@
 zen.section5.0506 の Docstring
 PP_05_06_変数のスコープ.ipynb
 '''
+# local
+def number():
+  num = [1, 2, 3, 4]
+  print(num)
+
+number()
+
+
+
+# grobal
+fruits = ["apple", "banana", "orange"]
+def fruits_kinds():
+  print(fruits)
+
+fruits_kinds()
