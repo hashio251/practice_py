51d7099ca8ca174a361b7afc935147bc7271d182
Yuka Hashio <251zs03753@student.zen.ac.jp>
2026-02-26 12:46:10 +0900

    add code 0604.py

--- zen/section6/0604.py
+++ zen/section6/0604.py
@@ -4,18 +4,22 @@ PP_06_04_リストのメソッド.ipynb
 '''
 
 
-
 # reverse
 '''
 reverse メソッドを使って、 iroha リストの要素を逆順に並べ替えてください。
 リストを表示して、中身を確かめてください。
 '''
+iroha = ['い', 'と', 'に', 'は', 'へ', 'ほ', 'ろ']
+iroha.sort(reverse=True)
+print(iroha)
 
 
 
 # index
-
+print(iroha.index('と'))
 
 
 
 # copy
+iroha_cp = iroha.copy()
+print(iroha_cp)
