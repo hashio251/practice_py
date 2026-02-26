6bcd6133bcfe881f068682819b3b8d035e1fdef0
Yuka Hashio <251zs03753@student.zen.ac.jp>
2026-02-26 12:21:06 +0900

    add code 0602.py

--- zen/section6/0602.py
+++ zen/section6/0602.py
@@ -11,7 +11,7 @@ numbers = ["one", "two", "three", "four", "five", "six", "seven"]
 インデックスを使って、 "three" を参照してください。
 '''
 numbers = ["one", "two", "three", "four", "five", "six", "seven"]
-
+print(numbers[2])
 
 
 
@@ -19,7 +19,7 @@ numbers = ["one", "two", "three", "four", "five", "six", "seven"]
 '''
 負のインデックスを使って、numbers リストの最後の要素を参照してください。
 '''
-
+print(numbers[-1])
 
 
 
@@ -28,7 +28,7 @@ numbers = ["one", "two", "three", "four", "five", "six", "seven"]
 スライスを使って、numbers リストの 2 番目から 5 番目の要素を取り出してください。
 5 番目の要素である "five" も含めてください。
 '''
-
+print(numbers[1:5])
 
 
 
@@ -36,7 +36,7 @@ numbers = ["one", "two", "three", "four", "five", "six", "seven"]
 '''
 numbers リストの、最初の 4 個の要素を取り出してください。
 '''
-
+print(numbers[:4])
 
 
 
@@ -44,9 +44,18 @@ numbers リストの、最初の 4 個の要素を取り出してください。
 '''
 numbers リストの、 4 番目の要素以降をすべて取り出してください。
 '''
-
+print(numbers[3:])
 
 
 '''
 numbers リストの、最後の 3 個の要素を取り出してください。
 '''
+print(numbers[-3:])
+
+
+
+'''
+スライスを使って、numbers リストから、1 番目、4 番目、7 番目の要素を取り出してください。
+要素のインデックスは 3 ずつ増えています。
+'''
+print(numbers[::3])
